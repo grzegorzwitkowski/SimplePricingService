10798c015e22f164f32c1dc5ca9be7a5576a0888
Grzegorz Witkowski <grzegorz.witkowski@allegrogroup.com>
2015-01-02 15:59:04 +0100

    running tests separately

--- src/test/java/pricingservice/AcceptanceTest.java
+++ src/test/java/pricingservice/AcceptanceTest.java
@@ -1,18 +1,21 @@
 package pricingservice;
 
-import com.google.common.collect.ImmutableList;
 import org.jbehave.core.InjectableEmbedder;
 import org.jbehave.core.annotations.Configure;
 import org.jbehave.core.annotations.UsingEmbedder;
 import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.io.CodeLocations;
-import org.jbehave.core.io.StoryPathResolver;
+import org.jbehave.core.io.StoryFinder;
 import org.jbehave.core.junit.AnnotatedEmbedderRunner;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.net.URL;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static org.jbehave.core.reporters.Format.CONSOLE;
 import static org.jbehave.core.reporters.Format.HTML;
@@ -20,7 +23,7 @@ import static org.jbehave.core.reporters.Format.HTML;
 @RunWith(AnnotatedEmbedderRunner.class)
 @Configure(storyReporterBuilder = AcceptanceTest.MyReportBuilder.class)
 @UsingEmbedder(embedder = Embedder.class, verboseFailures = true, storyTimeoutInSecs = 60)
-public abstract class AcceptanceTest extends InjectableEmbedder {
+public class AcceptanceTest extends InjectableEmbedder {
 
     public static class MyReportBuilder extends StoryReporterBuilder {
         public MyReportBuilder() {
@@ -31,8 +34,25 @@ public abstract class AcceptanceTest extends InjectableEmbedder {
 
     @Test
     public void run() {
-        StoryPathResolver storyPathResolver = injectedEmbedder().configuration().storyPathResolver();
-        String storyPath = storyPathResolver.resolve(this.getClass());
-        injectedEmbedder().runStoriesAsPaths(ImmutableList.of(storyPath));
+        List<String> storyPaths = findStoryPathForClass(getClass());
+
+        injectedEmbedder().runStoriesAsPaths(storyPaths);
+    }
+
+    private List<String> findStoryPathForClass(Class<? extends AcceptanceTest> acceptanceTestClass) {
+        String acceptanceTestClassName = acceptanceTestClass.getSimpleName();
+        Pattern pattern = Pattern.compile("([A-Z].*?)([A-Z]|\\z)");
+        Matcher matcher = pattern.matcher(acceptanceTestClassName);
+        int startAt = 0;
+        StringBuilder builder = new StringBuilder();
+        while (matcher.find(startAt)) {
+            builder.append(matcher.group(1).toLowerCase());
+            builder.append("_");
+            startAt = matcher.start(2);
+        }
+        String storyNameString = builder.substring(0, builder.length() - 1) + ".story";
+
+        return new StoryFinder().findPaths(CodeLocations.codeLocationFromPath("src/test/resources/"), "**/*.story", "")
+                .stream().filter(path -> path.endsWith(storyNameString)).collect(Collectors.toList());
     }
 }
